e0e46a69bb7090fb222f1044b04b4d904a8d24e4
Cellxi <55945370+Cellxi@users.noreply.github.com>
2025-04-13 15:56:08 +0800

    Add annotations to simple.c

--- Chapter_2/ProgrammingProjects/simple.c
+++ Chapter_2/ProgrammingProjects/simple.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+//  You must declare the parameter as void explicitly rather than omit it
 int simple_init(void) {
   printk(KERN_INFO "Loading Kernel Module\n");
   return 0;
@@ -9,9 +10,11 @@ int simple_init(void) {
 
 void simple_exit(void) { printk(KERN_INFO "Removing Kernel Module\n"); }
 
-module_init(simple_init);
-module_exit(simple_exit);
+//  These two macros are defined in init.h
+module_init(simple_init);  //  it tells kernel where to start this module
+module_exit(simple_exit);  //  it tells kernel where to exit it
 
+//  These three are defined in module.h
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
 MODULE_AUTHOR("Cellxi");
