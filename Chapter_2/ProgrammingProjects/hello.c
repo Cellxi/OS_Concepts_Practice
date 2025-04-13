1fa03cc97dcd5d7cde16847cd5d15a1d0c04b3c1
Cellxi <55945370+Cellxi@users.noreply.github.com>
2025-04-13 16:23:58 +0800

    Add annotations to hello.c

--- Chapter_2/ProgrammingProjects/hello.c
+++ Chapter_2/ProgrammingProjects/hello.c
@@ -4,9 +4,22 @@
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 
+/*
+    we need proc_create(), copy_to_user() and remove_proc_entry() in proc_fs.h
+*/
+
 #define BUFFER_SIZE 128
 #define PROC_NAME "hello"
 
+/*
+    __user tells kernel to allocate a pointer in user sapce of memory
+    loff_t = long offset type
+    pos represents the offset within the file
+    the proc_read declared here is a diy function
+    but there is another true proc_read in kernel
+    so we modify struct proc_ops to tell kernel to use our proc_ops
+    therefore we declare it with static, limiting its visibility to this code file
+*/
 ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count,
                   loff_t *pos);
 static struct proc_ops proc_ops = {
@@ -24,6 +37,8 @@ ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count,
                   loff_t *pos) {
   int rv = 0;
   char buffer[BUFFER_SIZE];
+
+  //  ensure the read operation is done only once
   static int completed = 0;
   if (completed) {
     completed = 0;
